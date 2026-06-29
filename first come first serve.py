84901012d5d82694b80a30fa913f06a5e9ce3937
mandlenkosimpande-web <mandlenkosimpande@gmail.com>
2026-06-29 18:41:08 +0400

    your message here

--- first come first serve.py
+++ first come first serve.py
@@ -1,8 +1,10 @@
+import threading
+
 #first come first served is a CPU scheduling algorithm. processes are executed in the oder they arrive with no preemption.
 def fcfs(processes):
     print("\n--- FCFS CPU Scheduling ---\n")
-    print(f"{'Process':<}{"Burst Time":<15}{"Waiting Time":<15}{"Turnaround Time"}")
-    print("-" * 50)
+    print(f"{'Process':<10}{"Burst Time":<15}{"Waiting Time":<15}{"Turnaround Time"}")
+    print("-" * 50) #primts '-' 50 times
 
     #defining the the tracking variables 
 
@@ -24,4 +26,22 @@ def fcfs(processes):
         total_turnaround += turnaround
         waiting_time += burst_time #the following process will wait for this one to finish
 
+    n = len(processes)
+    print("-" * 50)
+    print(f"\nAverage Waiting Time :{total_waiting / n:.2f}")
+    print(f"Average Turnaround Time :{total_turnaround / n:.2f}")
+
+def get_input():
+    processes = []
+    n = int(input("How many processes?")) # it asks the user how many proceses they would like to enter 
+    for i in range(n):
+        burst_time = int(input(f"Enter Burst Time for process {i+1}: "))
+        processes.append((i+1, burst_time))
+    return(processes)
+
+#creating a thread so that fcfs is run inside it 
+processes = get_input()
+thread = threading.Thread(target=fcfs, args=(processes,))
+thread.start()
+thread.join()
 
